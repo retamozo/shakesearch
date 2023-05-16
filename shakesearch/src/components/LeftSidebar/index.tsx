73a3b1c43a231314f5c9bda5a13c5bd4afd8dce7
retamozo <eretamozo97@gmail.com>
2023-05-16 18:02:05 -0300

    added placeholder

--- shakesearch/src/components/LeftSidebar/index.tsx
+++ shakesearch/src/components/LeftSidebar/index.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import Header from "../Header";
 
 const LeftSidebar = () => {
-  return <div>contenido</div>;
+  return (
+    <div>
+      contenido
+      <p>aca tiene q ir todo el coso de los capitulos y eso</p>
+    </div>
+  );
 };
 
 export default LeftSidebar;
