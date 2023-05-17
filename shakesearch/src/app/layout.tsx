96cf08fb51ec4a47b4913bc8c5aecd6b5155d028
retamozo <eretamozo97@gmail.com>
2023-05-17 02:18:20 -0300

    layout changes.

--- shakesearch/src/app/layout.tsx
+++ shakesearch/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Lora } from "next/font/google";
 import Header from "@/components/Header";
 import ChakraProvider from "../providers/ChakraProvider";
 import TextBoardProvider from "@/providers/TextBoardProvider";
+import { LeftSidebar, RightSidebar } from "@/components";
 
 const lora = Lora({
   subsets: ["latin"],
@@ -17,12 +18,36 @@ export default function RootLayout({
   return (
     <html lang="en">
       <title>Shakesearh app</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <body className={lora.className}>
         <header className="px-6 pt-6">
           <Header />
         </header>
         <ChakraProvider>
-          <TextBoardProvider>{children}</TextBoardProvider>
+          <TextBoardProvider>
+            <main className="flex container mx-auto">
+              <LeftSidebar />
+              {children}
+              <RightSidebar />
+            </main>
+          </TextBoardProvider>
         </ChakraProvider>
       </body>
     </html>
