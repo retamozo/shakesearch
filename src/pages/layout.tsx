85c2b4b5d656aa469abb590b86c8661c2caaa3a9
retamozo <eretamozo97@gmail.com>
2023-07-20 00:19:09 -0300

    removing unnecesary contexts. Actually doing the right thing lol

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,7 +1,5 @@
 import { Lora } from "next/font/google";
 import Header from "@/components/Header";
-import ChakraProvider from "../providers/ChakraProvider";
-import TextBoardProvider from "@/providers/TextBoardProvider";
 import { ReactNode } from "react";
 import { Footer, WorkIndex } from "@/components";
 
@@ -14,7 +12,7 @@ type RootLayoutProps = {
 };
 
 const scroll =
-  "md:w-full md:overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 scrollbar-thumb-rounded md:pr-12";
+  "md:w-full md:overflow-y-auto md:overflow-x-hidden scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 scrollbar-thumb-rounded md:pr-12";
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
@@ -38,32 +36,29 @@ export default function RootLayout({ children }: RootLayoutProps) {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
-      <ChakraProvider>
-        <TextBoardProvider>
-          <body className={`${lora.className} h-screen flex flex-col`}>
-            <header className="px-6 pt-6 md:container md:mx-auto">
-              <Header />
-            </header>
-            <main className="pt-2 flex-grow md:container md:mx-auto md:h-4/5">
-              <div className="flex w-full md:justify-between h-3/4 md:h-full">
-                <div className="flex flex-col md:flex-row md:w-full">
-                  <div className="hidden md:flex md:w-2/5">
-                    <div className={scroll}>
-                      <WorkIndex />
-                    </div>
-                  </div>
-                  <div className="px-6 md:flex md:w-3/5">
-                    <div className={scroll}>{children}</div>
-                  </div>
+
+      <body className={`${lora.className} h-screen flex flex-col`}>
+        <header className="px-6 pt-6 md:container md:mx-auto">
+          <Header />
+        </header>
+        <main className="pt-2 flex-grow md:container md:mx-auto md:h-4/5">
+          <div className="flex w-full md:justify-between h-3/4 md:h-full">
+            <div className="flex flex-col md:flex-row md:w-full">
+              <div className="hidden md:flex md:w-2/5">
+                <div className={scroll}>
+                  <WorkIndex />
                 </div>
               </div>
-            </main>
-            <footer className="h-10 py-2">
-              <Footer />
-            </footer>
-          </body>
-        </TextBoardProvider>
-      </ChakraProvider>
+              <div className="px-6 md:flex md:w-3/5">
+                <div className={scroll}>{children}</div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <footer className="h-10 py-2">
+          <Footer />
+        </footer>
+      </body>
     </html>
   );
 }
