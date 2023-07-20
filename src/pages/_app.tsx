9085c95b72739be7dadcf8af6782db81b6bba1f5
retamozo <eretamozo97@gmail.com>
2023-07-20 00:14:47 -0300

    lifting provider up : now `TextBoardProvider` is on top of the app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,72 +1,71 @@
 import Head from "next/head";
 import Layout from "@/layout";
 import type { AppProps } from "next/app";
-import TextBoardProvider from "@/providers/TextBoardProvider";
 import { ChakraProvider } from "@chakra-ui/react";
+import TextBoardProvider from "@/providers/TextBoardProvider";
+
 import "./globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log("pageProps", pageProps);
   return (
     <ChakraProvider>
-      <TextBoardProvider>
-        <Head>
-          <title>Shakesearch</title>
-          <meta name="title" content="Shakesearch" />
-          <meta
-            name="description"
-            content="A minimal, clean and beautiful website to help Shakespeare fans to find your beautiful works."
-          />
+      <Head>
+        <title>Shakesearch</title>
+        <meta name="title" content="Shakesearch" />
+        <meta
+          name="description"
+          content="A minimal, clean and beautiful website to help Shakespeare fans to find your beautiful works."
+        />
 
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://shakesearch.vercel.app/" />
-          <meta property="og:title" content="Shakesearch" />
-          <meta
-            property="og:description"
-            content="A minimal, clean and beautiful website to help Shakespeare fans to find your beautiful works."
-          />
-          <meta
-            property="og:image"
-            content="https://i.imgur.com/vNg2OmF.jpeg"
-          />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://shakesearch.vercel.app/" />
+        <meta property="og:title" content="Shakesearch" />
+        <meta
+          property="og:description"
+          content="A minimal, clean and beautiful website to help Shakespeare fans to find your beautiful works."
+        />
+        <meta property="og:image" content="https://i.imgur.com/vNg2OmF.jpeg" />
 
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta
-            property="twitter:url"
-            content="https://shakesearch.vercel.app/"
-          />
-          <meta property="twitter:title" content="Shakesearch" />
-          <meta
-            property="twitter:description"
-            content="A minimal, clean and beautiful website to help Shakespeare fans to find your beautiful works."
-          />
-          <meta
-            property="twitter:image"
-            content="https://i.imgur.com/vNg2OmF.jpeg"
-          />
-          <link rel="icon" href="/favicon.ico" />
-          <link
-            rel="stylesheet preload prefetch"
-            href="/fonts/poppins-v12-latin-300.woff2"
-            as="style"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="stylesheet preload prefetch"
-            href="/fonts/poppins-v12-latin-600.woff2"
-            as="style"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="stylesheet preload prefetch"
-            href="/fonts/poppins-v12-latin-regular.woff2"
-            as="style"
-            crossOrigin="anonymous"
-          />
-        </Head>
-        <Layout>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://shakesearch.vercel.app/"
+        />
+        <meta property="twitter:title" content="Shakesearch" />
+        <meta
+          property="twitter:description"
+          content="A minimal, clean and beautiful website to help Shakespeare fans to find your beautiful works."
+        />
+        <meta
+          property="twitter:image"
+          content="https://i.imgur.com/vNg2OmF.jpeg"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet preload prefetch"
+          href="/fonts/poppins-v12-latin-300.woff2"
+          as="style"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet preload prefetch"
+          href="/fonts/poppins-v12-latin-600.woff2"
+          as="style"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet preload prefetch"
+          href="/fonts/poppins-v12-latin-regular.woff2"
+          as="style"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Layout>
+        <TextBoardProvider content={pageProps.content}>
           <Component {...pageProps} />
-        </Layout>
-      </TextBoardProvider>
+        </TextBoardProvider>
+      </Layout>
     </ChakraProvider>
   );
 }
